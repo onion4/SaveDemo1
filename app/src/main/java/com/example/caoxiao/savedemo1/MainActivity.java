215241fa21bedbd8a2ad63ef2fa45ff640afb2e7
onion4 <caoxiao90@aliyun.com>
2015-10-17 11:43:47 +0800

    123

--- app/src/main/java/com/example/caoxiao/savedemo1/MainActivity.java
+++ app/src/main/java/com/example/caoxiao/savedemo1/MainActivity.java
@@ -90,7 +90,7 @@ public class MainActivity extends ActionBarActivity {
         Log.i("onRestore",string);
         Log.i("onRestoreInstanceState","onRestoreInstanceState");
     }
-
+//
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
